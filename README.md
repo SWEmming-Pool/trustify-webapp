3d651335766fd853347c76a928de300a2f90a186
Ennio Italiano <ennio.italiano@gmail.com>
2023-03-24 10:42:33 +0100

    Rinominato progetto 2

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TrustifyFrontend
+# TrustifyWebApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.1.5.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'trustify-frontend';
+  title = 'trustify-webapp';
 }
