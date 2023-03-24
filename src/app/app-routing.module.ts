210d7606a2509d77fcf3ddc38de19c607fcff72c
Ennio Italiano <ennio.italiano@gmail.com>
2023-03-24 12:03:35 +0100

    Creato componente lista pagamenti e aggiunto routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaymentsComponent } from './components/payments/payments.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'payments', component: PaymentsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
