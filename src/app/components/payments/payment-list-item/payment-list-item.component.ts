ef93907f8ad26d103cad39d376e7ec305a41e013
Ennio Italiano <ennio.italiano@gmail.com>
2023-03-25 11:55:35 +0100

    Modifiche visualizzazione pagamenti

--- src/app/components/payments/payment-list-item/payment-list-item.component.ts
+++ src/app/components/payments/payment-list-item/payment-list-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Payment } from '../Payments';
-import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { faStar, faCreditCard } from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-payment-list-item',
@@ -9,6 +9,7 @@ import { faStar } from '@fortawesome/free-regular-svg-icons';
 })
 export class PaymentListItemComponent {
   faStar = faStar;
+  faCreditCard = faCreditCard;
   dateToString(date: Date): string {
     return date.toISOString().split('T')[0];
   }
