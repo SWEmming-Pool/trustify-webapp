6920f7c0aa13ab3b8be1d1d2d044da815429010f
Ennio <ennio.italiano@gmail.com>
2023-04-09 11:09:14 +0200

    Migliorata visualizzazione voto

--- src/app/components/reviews/review-list-item/review-list-item.component.ts
+++ src/app/components/reviews/review-list-item/review-list-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Review } from '../Reviews';
 import { faStar, faUser } from '@fortawesome/free-regular-svg-icons';
+import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-review-list-item',
@@ -9,6 +10,7 @@ import { faStar, faUser } from '@fortawesome/free-regular-svg-icons';
 })
 export class ReviewListItemComponent {
   faStar = faStar;
+  faStarSolid = faStarSolid;
   faUser = faUser;
 
   @Input() review!: Review;
