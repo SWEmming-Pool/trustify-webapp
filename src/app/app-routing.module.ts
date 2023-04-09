45d36102fb347317efe52d8f41ad367bf9f0c781
Ennio <ennio.italiano@gmail.com>
2023-04-09 12:05:17 +0200

    Inizio bozza rilascio recensione

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { UserComponent } from './components/user/user.component';
 import { HomeComponent } from './components/home/home.component';
 import { TransactionsComponent } from './components/transactions/transactions.component';
 import { ReviewsComponent } from './components/reviews/reviews.component';
+import { LeaveReviewComponent } from './components/reviews/leave-review/leave-review.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'transactions', component: TransactionsComponent },
   { path: 'user', component: UserComponent },
   { path: 'reviews', component: ReviewsComponent },
+  { path: 'leave-review', component: LeaveReviewComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
--- src/app/components/reviews/leave-review/leave-review.component.ts
+++ src/app/components/reviews/leave-review/leave-review.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { faStar, faUser } from '@fortawesome/free-regular-svg-icons';
+import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-leave-review',
   templateUrl: './leave-review.component.html',
-  styleUrls: ['./leave-review.component.scss']
+  styleUrls: ['./leave-review.component.scss'],
 })
 export class LeaveReviewComponent {
-
+  faStar = faStar;
+  faStarSolid = faStarSolid;
+  faUser = faUser;
 }
