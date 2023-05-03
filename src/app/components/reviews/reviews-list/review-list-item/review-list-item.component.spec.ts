efea73c75ed837acc4b7aa40947243b173b5205f
trincaw <nikxtr@gmail.com>
2023-05-03 21:33:57 +0200

    fix

--- src/app/components/reviews/reviews-list/review-list-item/review-list-item.component.spec.ts
+++ src/app/components/reviews/reviews-list/review-list-item/review-list-item.component.spec.ts
@@ -1,23 +1,49 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ReviewListItemComponent } from './review-list-item.component';
-
+import { faStar, faUser } from '@fortawesome/free-regular-svg-icons';
+import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons';
+import { Review } from '../../Review';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 describe('ReviewListItemComponent', () => {
   let component: ReviewListItemComponent;
   let fixture: ComponentFixture<ReviewListItemComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReviewListItemComponent ]
+      imports: [FontAwesomeModule],
+      declarations: [ReviewListItemComponent]
     })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(ReviewListItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      .compileComponents();
   });
 
-  it('should create', () => {
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReviewListItemComponent);
+    component = fixture.componentInstance;
+    component.review = new Review(0, '', 0, '', '');
+    fixture.detectChanges();
+  });
+  it('should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the correct star icons', () => {
+    expect(component.faStar).toEqual(faStar);
+    expect(component.faStarSolid).toEqual(faStarSolid);
+  });
+
+  it('should set the correct user icon', () => {
+    expect(component.faUser).toEqual(faUser);
+  });
+
+  it('should initialize the stars array with the correct number of stars', () => {
+    expect(component.stars.length).toEqual(5);
+  });
+
+  it('should set the correct number of stars based on the review rating', () => {
+    expect(component.stars[0]).toBeFalse();
+    expect(component.stars[1]).toBeFalse();
+    expect(component.stars[2]).toBeFalse();
+    expect(component.stars[3]).toBeFalse();
+    expect(component.stars[4]).toBeFalse();
   });
 });
--- src/app/services/contract.service.spec.ts
+++ src/app/services/contract.service.spec.ts
@@ -3,6 +3,21 @@ import Web3 from 'web3';
 import { ContractService } from './contract.service';
 import { Transaction } from '../components/transactions/Transaction';
 
+class MockContract {
+  static methods = {
+    getUnreviewedTransactions: (address: string) => ({
+      call: (callback: Function) => {
+        const transactions = [
+
+          ["0x403a043ea3d4d894881ff51b0780eeb98fbbca8dfdf270b23b8b778f9d11ea5a", 1683120461, 10, "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4"]
+
+        ];
+        callback(null, transactions);
+      }
+    })
+  };
+}
+
 describe('ContractService', () => {
   let service: ContractService;
 
@@ -13,28 +28,19 @@ describe('ContractService', () => {
       ],
     });
 
-    // Mock the getUnreviewedTransactions method
-    spyOn(ContractService.Contract.methods, 'getUnreviewedTransactions').and.returnValue({
-      call: (callback: any) => {
-        callback(null, [
-          ["0x36Ac550A5BD2b6a30f52901b3B63ede555F0fdF1", "0x36Ac550A5BD2b6a30f52901b3B63ede555F0fdF1", "10", "123", "1"]
-        ]);
-      }
-    });
 
-    // Get the instance of the ContractService
-    service = TestBed.inject(ContractService);
   });
 
   it('should return an array of transactions', async () => {
     // Arrange
     const address = '0x1234567890abcdef';
 
+    ContractService.Contract = MockContract;
     // Act
     const result = await ContractService.getUnreviewedTransactions(address);
 
     // Assert
-    const arr: Transaction[] = [new Transaction("1", 123, 10, "0x36Ac550A5BD2b6a30f52901b3B63ede555F0fdF1", "0x36Ac550A5BD2b6a30f52901b3B63ede555F0fdF1")];
+    const arr: Transaction[] = [new Transaction("0x403a043ea3d4d894881ff51b0780eeb98fbbca8dfdf270b23b8b778f9d11ea5a", 1683120461, 10, "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4", "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4")];
     expect(result).toEqual(arr);
   });
 });
